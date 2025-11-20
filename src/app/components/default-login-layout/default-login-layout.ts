61d94fd51a8f3494b635783b97e90d1c19b4f507
brenofs1997 <brenofs1997@gmail.com>
2025-11-20 20:42:15 -0300

    feat: creating initial structure

---
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [DefaultLoginLayout],
+  templateUrl: './default-login-layout.html',
+  styleUrl: './default-login-layout.css',
+})
+export class DefaultLoginLayout {
+
+}
